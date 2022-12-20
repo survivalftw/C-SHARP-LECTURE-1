abc03fc9b1a44a835ca45f9a1b269188125dbff5
Anton Kommer <lipokuza@rambler.ru>
2022-12-20 05:25:57 +0300

    add: Task 002

---
+++ C#LESSON_1/Task_002/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter a and b");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+if(a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+    Console.Write("min = ");
+    Console.WriteLine(b);
+}
+if(a == b)
+{
+    Console.Write("max = min = ");
+    Console.WriteLine(a);
+}
+if(a < b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+    Console.Write("min = ");
+    Console.WriteLine(a);
+}
