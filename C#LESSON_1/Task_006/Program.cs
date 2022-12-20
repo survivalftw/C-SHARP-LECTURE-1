a9e1891c2724dd0fe37c6dbe31732f7a2aaf5727
Anton Kommer <lipokuza@rambler.ru>
2022-12-20 05:51:19 +0300

    add: Task 006

---
+++ C#LESSON_1/Task_006/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter a number");
+
+int a = int.Parse(Console.ReadLine());
+
+int b = (a % 2) * (a % 2);
+
+if(b == 0)
+{
+    Console.Write("Number = ");
+    Console.WriteLine(a);
+    Console.Write("is even");
+}
+if(b == 1)
+{
+    Console.Write("Number = ");
+    Console.WriteLine(a);
+    Console.Write("is odd");
+}
