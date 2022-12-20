ec6a3b35032f058cbeb03cc074bad1533e9fc82e
Anton Kommer <lipokuza@rambler.ru>
2022-12-20 04:28:29 +0300

    add: Example 005 Condition If Else

---
+++ C#LECTURE_1/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter username");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Adriana Chechik");
+{
+    Console.WriteLine("Don't jump kekw");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
