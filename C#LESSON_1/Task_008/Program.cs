5636765725825523ef09298c566e5585b64e991e
Anton Kommer <lipokuza@rambler.ru>
2022-12-20 06:21:07 +0300

    add: Task 008

---
+++ C#LESSON_1/Task_008/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Enter N");
+
+int N = int.Parse(Console.ReadLine());
+int number = 1;
+if (N > 0)
+{
+    while (number <= N)
+    {
+    int evencheck = number % 2;
+    if (evencheck == 0)
+    {
+        Console.Write(number);
+        Console.Write(" ");         
+    }
+    number++;
+    }
+}
+else
+{
+    while (N <= number)
+    {
+    int evencheck = number % 2;
+    if (evencheck == 0)
+    {
+        Console.Write(number);
+        Console.Write(" ");         
+    }
+    number--;
+    }
+}
