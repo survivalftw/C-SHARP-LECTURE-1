17824e6f9eec022897e2389e392260fe9a448385
Anton Kommer <lipokuza@rambler.ru>
2022-12-20 04:04:35 +0300

    add: RNG new Random stuff

--- C#LECTURE_1/Example003_Sum/Program.cs
+++ C#LECTURE_1/Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int nubmerB = 5;
-int result = numberA + nubmerB;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
 Console.WriteLine(result);
