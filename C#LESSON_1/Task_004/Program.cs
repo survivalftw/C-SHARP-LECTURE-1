fc1b11f030007cd8caacf8f3ca296bc89d73fd0a
Anton Kommer <lipokuza@rambler.ru>
2022-12-20 05:36:04 +0300

    add: Task 004

---
+++ C#LESSON_1/Task_004/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter a, b and c");
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
