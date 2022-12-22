7cf64899c2345fa4d1103b6c191d89b73ec4b1a7
Anton Kommer <lipokuza@rambler.ru>
2022-12-23 02:02:01 +0300

    add: task_010

---
+++ C#LESSON_2/Task_010/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter a number");
+
+int N = int.Parse(Console.ReadLine());
+
+int a = N / 100;
+
+int b = N - a * 100;
+int c = b / 10;
+if (N > 0)
+{
+    Console.WriteLine(c);
+}
+else
+{
+    Console.WriteLine(-c);
+}
