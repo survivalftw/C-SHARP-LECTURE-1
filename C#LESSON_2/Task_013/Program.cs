c11e164865c3e4a51c76036a88771330c48a22b6
Anton Kommer <lipokuza@rambler.ru>
2022-12-23 03:51:43 +0300

    add: Task_013

---
+++ C#LESSON_2/Task_013/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Enter a Number");
+
+int Number = int.Parse(Console.ReadLine());
+int a = Number;
+int Counter = 0;
+int FirstDigit = 0;
+int SecondDigit = 0;
+
+while (a/10 != 0)
+{
+    FirstDigit = a/10;
+    SecondDigit = a - (a/10) * 10;    
+    a = a / 10;
+    Counter = Counter + 1;
+}
+
+if (Counter > 1)
+{
+    int ThirdDigit = (Number - FirstDigit * Convert.ToInt32(Math.Pow(10, Counter)) - SecondDigit * Convert.ToInt32(Math.Pow(10, Counter-1)))/Convert.ToInt32(Math.Pow(10, Counter-2));
+    if (Number > 0)
+    {
+        Console.WriteLine(ThirdDigit);
+    }
+    else
+    {
+        Console.WriteLine(-ThirdDigit);
+    }
+}
+else
+{
+    Console.WriteLine("There is no ThirdDigit");
+}
