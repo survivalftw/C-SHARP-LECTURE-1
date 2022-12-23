f6f16d8c58cccbd16c5095b9a7b431ffe48ae42d
Anton Kommer <lipokuza@rambler.ru>
2022-12-23 04:33:39 +0300

    add: Task_015

---
+++ C#LESSON_2/Task_015/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter a Number");
+
+int Weekend = int.Parse(Console.ReadLine());
+
+if (Weekend < 8 && Weekend > 0)
+{
+    if (Weekend == 6 || Weekend == 7)
+    {
+    Console.WriteLine("Holyday");
+    }
+    else
+    {
+    Console.WriteLine("Weekday");
+    }
+}
+else
+{
+    Console.WriteLine("Error");
+}
