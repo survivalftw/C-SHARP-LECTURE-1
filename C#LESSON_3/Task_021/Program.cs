8f972659ad130df12f56ae7bda730e24cbf83ad4
Anton Kommer <lipokuza@rambler.ru>
2022-12-27 05:02:15 +0300

    add: Task_021

--- C#LESSON_3/Task_021/Program.cs
+++ C#LESSON_3/Task_021/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ReadNumber(string text) {
+Console.WriteLine(text);
+int number;
+string readNumber = Console.ReadLine() ?? "Nullable";
+if (int.TryParse(readNumber, out number)) {}
+else
+{
+    Console.WriteLine("Enter digits only");
+    number = ReadNumber (text);
+}
+return number;
+}
+
+
+int [] coordinateA = new int [3];
+coordinateA = ReadCoordinate("Enter coordinate A");
+int [] coordinateB = new int [3];
+coordinateB = ReadCoordinate("Enter coordinate B");
+
+Console.WriteLine("Distance between A and B = " + GetLengh(coordinateA, coordinateB));
+
+int [] ReadCoordinate(string text) {
+    Console.WriteLine(text);
+    int [] number = new int [3];
+
+    int x = ReadNumber("Enter x coordinate");
+    int y = ReadNumber("Enter y coordinate");
+    int z = ReadNumber("Enter z coordinate");
+    number [0] = x;
+    number [1] = y;
+    number [2] = z;
+
+    return number;
+}
+
+double GetLengh(int[] a, int[] b) {
+    int lengthX = Math.Abs(a[0] - b[0]);
+    int lengthY = Math.Abs(a[1] - b[1]);
+    int lengthZ = Math.Abs(a[2] - b[2]);
+
+    double lenghtTASK = Math.Sqrt(lengthX * lengthX + lengthY * lengthY + lengthZ * lengthZ);
+
+    return lenghtTASK;
+}
