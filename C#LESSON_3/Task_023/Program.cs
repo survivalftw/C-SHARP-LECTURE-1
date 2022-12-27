44a67b6cc8300909cd2bb1bac67d56741644b36f
Anton Kommer <lipokuza@rambler.ru>
2022-12-27 05:50:40 +0300

    add: Task_023

--- C#LESSON_3/Task_023/Program.cs
+++ C#LESSON_3/Task_023/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter a Number");
+
+int Number = int.Parse(Console.ReadLine());
+int a = 1;
+int Cubed = 0;
+
+if (Number < 0)
+{
+        while (Number < a)
+    {
+        Cubed = Convert.ToInt32(Math.Pow(a, 3));
+        Console.Write(Cubed + ", ");
+        a = a - 1;
+    }
+    if (Number == a)
+    {
+        Cubed = Convert.ToInt32(Math.Pow(a, 3));
+        Console.Write(Cubed);
+    }
+}
+else
+{
+    while (Math.Abs(Number) > a)
+    {
+        Cubed = Convert.ToInt32(Math.Pow(a, 3));
+        Console.Write(Cubed + ", ");
+        a = a + 1;
+    }
+    if (Math.Abs(Number) == a)
+    {
+        Cubed = Convert.ToInt32(Math.Pow(a, 3));
+        Console.Write(Cubed);
+    }
+}
