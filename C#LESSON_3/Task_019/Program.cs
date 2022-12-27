dfe28858413ac2ce7818aace0c93801fd8ac9664
Anton Kommer <lipokuza@rambler.ru>
2022-12-27 04:05:54 +0300

    add: Task_021

---
+++ C#LESSON_3/Task_019/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter a Number");
+
+int Number = int.Parse(Console.ReadLine());
+int a = Number;
+int Counter = 0;
+int FirstDigit = 0;
+int SecondDigit = 0;
+
+while (a/10 != 0)
+{
+    FirstDigit = a/10;
+    SecondDigit = a - (a/10) * 10;    
+    a = a / 10;
+    Counter = Counter + 1;
+}
