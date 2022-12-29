996f1b94738ae7fe1720e06ef06c80a9561559e9
Anton Kommer <lipokuza@rambler.ru>
2022-12-30 00:35:10 +0300

    add: Task_025

---
+++ C#LESSON_4/Task_025/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Enter base A");
+
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter power B");
+
+int B = int.Parse(Console.ReadLine());
+
+int Pow = GetExponentiationBaseAPowerB(A, B);
+
+Console.WriteLine(Pow);
+
+int GetExponentiationBaseAPowerB(int A, int B)
+{
+    int Pow = 1;
+    for (var i = 1; i <= B; i++)
+    {
+        Pow = Pow * A;
+    }
+
+    return Pow;
+}
