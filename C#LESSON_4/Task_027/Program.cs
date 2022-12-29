5961d9b9f038ae0694775b5a47b923aae9832d65
Anton Kommer <lipokuza@rambler.ru>
2022-12-30 02:12:45 +0300

    add: Task_027

--- C#LESSON_4/Task_027/Program.cs
+++ C#LESSON_4/Task_027/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter a Number");
+
+int Number = int.Parse(Console.ReadLine());
+int a = Number;
+int b = 0;
+int sum = 0;
+
+while (a / 10 != 0)
+{
+    b = a % 10;
+    sum = sum + b;
+    a = a / 10;
+}
+sum = Math.Abs(sum + a);
+
+Console.WriteLine("Number digit sum is " + sum);
