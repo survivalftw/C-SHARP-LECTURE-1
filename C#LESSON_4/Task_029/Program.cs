1957d8547208c7126e0531e559c31b97d007a717
Anton Kommer <lipokuza@rambler.ru>
2022-12-30 06:11:04 +0300

    add: Task_029 bad version

--- C#LESSON_4/Task_029/Program.cs
+++ C#LESSON_4/Task_029/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter Array size up to 8");
+int size = int.Parse(Console.ReadLine());
+
+Console.Write("Enter " + size);
+Console.Write(" Array elements and press enter " + (8-size));
+Console.Write(" times ");
+
+string A = Console.ReadLine();
+string B = Console.ReadLine();
+string C = Console.ReadLine();
+string D = Console.ReadLine();
+string E = Console.ReadLine();
+string F = Console.ReadLine();
+string G = Console.ReadLine();
+string H = Console.ReadLine();
+
+string[] ArrayText = new string[] {A,B,C,D,E,F,G,H};
+
+string[] result = CreateArray();
+PrintArray(result);
+
+string[] CreateArray()
+{
+    string[] array = new string [size];  
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = ArrayText[i];
+    }
+    return array;
+}
+
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (var i = 0; i < array.Length-1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write(array[array.Length-1]);
+    Console.Write("]");
+}
