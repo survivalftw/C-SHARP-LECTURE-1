4c420b5c110e7e5add0b47d9fa902866f455f817
Anton Kommer <lipokuza@rambler.ru>
2023-01-03 06:43:27 +0300

    add: Task_036

---
+++ C#LESSON_5/Task_036/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Enter Array size");
+int size = int.Parse(Console.ReadLine());
+int Count = 0;
+
+int[] result = CreateArray();
+
+PrintArray(result);
+CountOddIndexSumArray(result);
+
+int[] CreateArray()
+{  
+    Random random = new Random();
+    int[] array = new int [size];  
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-999,999);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (var i = 0; i < array.Length-1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write(array[array.Length-1]);
+    Console.Write("]");
+}
+
+void CountOddIndexSumArray(int[] array)
+{
+    for (var i = 1; i < array.Length; i += 2)
+        {
+            Count = Count + array[i];
+        }
+    Console.WriteLine(" ");
+    Console.Write("Odd index numbers sum is " + Count);
+}
