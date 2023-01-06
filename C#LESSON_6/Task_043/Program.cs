a810c7e21304847b7d8bdaddedc75288fe662390
Anton Kommer <lipokuza@rambler.ru>
2023-01-06 18:44:52 +0300

    add: Task_043

---
+++ C#LESSON_6/Task_043/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter k1 variable:\t");
+double k1 = double.Parse(Console.ReadLine());
+Console.Write("Enter b1 variable:\t");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("Enter k2 variable:\t");
+double k2 = double.Parse(Console.ReadLine());
+Console.Write("Enter b2 variable:\t");
+double b2 = double.Parse(Console.ReadLine());
+
+double x = (b2 - b1)/(k1 - k2);
+double y = (b2 - b1)/(k1 - k2) * k1 + b1;
+
+if (k1 == k2)
+{
+    Console.WriteLine("Two strait lines are paralleled");
+}
+else
+{
+    Console.Write("Cross coordinates are (" + x);
+    Console.Write("; " + y);
+    Console.Write(")");
+}
