132ad32721585848881a4d33b93f48e48129655d
Anton Kommer <lipokuza@rambler.ru>
2023-01-10 06:36:03 +0300

    add: Task_052

---
+++ C#LESSON_7/Task_052/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.Write("Enter Array m dimension size:\t");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Enter Array n dimension size:\t");
+int n = int.Parse(Console.ReadLine());
+
+int[,] array = GetArrayRandom(m,n);
+
+GetArrayRandom(m, n);
+PrintArray(array);
+Console.WriteLine("Arithmetic mean values are located below each corresponding column");
+FindArrayColumnArithmeticMean(array);
+
+int[,] GetArrayRandom(int m, int n)
+{
+Random random = new Random();
+int[,] array = new int [m,n];
+    for (var i = 0; i < array.GetLength(0); i += 1)
+    {
+        for (var j = 0; j < array.GetLength(1); j += 1)
+        {
+            array[i,j] = random.Next(0,99);
+        }
+    }
+return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i += 1)
+    {
+        for (int j = 0; j < array.GetLength(1); j += 1)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindArrayColumnArithmeticMean(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j += 1)
+    {
+        double counterCount = 0;
+        for (int i = 0; i < array.GetLength(0); i += 1)
+        {
+            counterCount = counterCount + array[i,j];
+        }
+        double columnMean = counterCount / array.GetLength(1);
+        Console.Write(columnMean);
+        Console.Write("; ");
+    }
+}
