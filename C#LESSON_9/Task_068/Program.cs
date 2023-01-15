dcd9251e7bbcabda3062edf37a823f8678d66637
Anton Kommer <lipokuza@rambler.ru>
2023-01-15 10:28:37 +0300

    add: Task_068

---
+++ C#LESSON_9/Task_068/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter M:\t");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Enter N:\t");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine(MethodAckerman(n,m));
+
+int MethodAckerman (int a, int b)
+{
+    if (a == 0)
+    return b + 1;
+
+    if ((a > 0) && (b == 0))
+    return MethodAckerman(a - 1, 1);
+
+    if ((b > 0) && (a > 0))
+    return MethodAckerman(a - 1, MethodAckerman(a, b - 1));
+return MethodAckerman (a, b);
+}
