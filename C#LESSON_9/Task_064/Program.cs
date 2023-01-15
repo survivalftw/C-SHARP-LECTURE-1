7189c066b6aaeacdcb5a506c77a7d65182325ce5
Anton Kommer <lipokuza@rambler.ru>
2023-01-15 08:02:24 +0300

    add: Task_064

--- C#LESSON_9/Task_064/Program.cs
+++ C#LESSON_9/Task_064/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter N:\t");
+int n = int.Parse(Console.ReadLine());
+if (n > 0)
+{
+    MyMethod(n);
+    void MyMethod(int i)
+    {
+    if (i == 0)
+    {
+        return;
+    }
+    Console.WriteLine(i);
+    i -= 1;
+    MyMethod(i);
+    }
+}
+else
+{
+    Console.WriteLine("N is <= 0");
+}
