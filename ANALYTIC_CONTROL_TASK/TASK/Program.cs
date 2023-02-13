9b97ce026c23cc5b7103a7c10552c74688a26947
Anton Kommer <lipokuza@rambler.ru>
2023-02-13 04:14:23 +0300

    add: initial code idea

--- ANALYTIC_CONTROL_TASK/TASK/Program.cs
+++ ANALYTIC_CONTROL_TASK/TASK/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int size = 8;
+Console.Write("Enter " + size);
+Console.Write(" string array elements and press Enter:\n");
+
+string A = Console.ReadLine();
+string B = Console.ReadLine();
+string C = Console.ReadLine();
+string D = Console.ReadLine();
+string E = Console.ReadLine();
+string F = Console.ReadLine();
+string G = Console.ReadLine();
+string H = Console.ReadLine();
+
+string[] InitialArray = new string[] {A,B,C,D,E,F,G,H};
+
+string[] result = CreateArray();
+PrintArray(result);
+
+string[] CreateArray()
+{
+    string[] array = new string [size];  
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = InitialArray[i];
+    }
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (var i = 0; i < array.Length-1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write(array[array.Length-1]);
+    Console.Write("]");
+}
+
+/*
+string[] CreateArrayLessThanThreeSymbols()
+{
+    string[] array = new string [size];  
+    for (var i = 0; i < array.Length; i++)
+    {
+        array[i] = InitialArray[i];
+    }
+    return array;
+}
+*/
