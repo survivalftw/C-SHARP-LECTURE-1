379adad5bcf09296c72d1daec4d0df371002f991
Anton Kommer <lipokuza@rambler.ru>
2023-03-09 02:46:09 +0300

    edit: Task_002

--- PYTHON_LESSON_1/Task_002.py
+++ PYTHON_LESSON_1/Task_002.py
@@ -1,8 +1,11 @@
-print("Input a number:")
+print("Input a 3-digit number:")
 n = int(input())
-sum = 0
-while n > 0:
-    x = n % 10
-    sum += x
-    n = n // 10
-print("The sum of number's numeric letters = ", sum)
+if int(n) < 100 or int(n) > 999:
+    print("Wrong number of digits, restart the program and enter 3-digit number")
+else:
+    sum = 0
+    while n > 0:
+        x = n % 10
+        sum += x
+        n = n // 10
+    print("The sum of number's digits = ", sum)
