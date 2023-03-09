060255a5e5dab00a022add5c9e74b8adfd159e7e
Anton Kommer <lipokuza@rambler.ru>
2023-03-09 03:26:26 +0300

    edit: Task_008

--- PYTHON_LESSON_1/Task_008.py
+++ PYTHON_LESSON_1/Task_008.py
@@ -1,7 +1,7 @@
 n = int(input('Input number of chocolate segments (lenght): '))
 m = int(input('Input number of chocolate segments (width): '))
 k = int(input('Input possible segment number of a chocolate piece after one line crack: '))
-if k >= n * m:
+if k >= n * m or k < 0:
     print ("It is not possible to line crack a piece with", k, "segments")
 elif k % n == 0 or k % m == 0:
     print("It is possible to line crack a piece with", k, "segments")
